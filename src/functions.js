af36ba9b5282f0908e8f0329d4869439a2096b8a
github-118610115 <github-118610115>
2024-12-28 12:43:10 +0000

    Edited files:     /src/functions.js

--- src/functions.js
+++ src/functions.js
@@ -12,6 +12,34 @@ function openWeatherMapCurrent(units, lang, q) {
     });
 }
 
+function openWeatherMapForecast(units, lang, q, date) {
+    // Формируем запрос к API
+    var url = "http://api.openweathermap.org/data/2.5/forecast?APPID=" + OPENWEATHERMAP_API_KEY + "&units=" + units + "&lang=" + lang + "&q=" + q;
+    
+    return $http.get(url, {
+        timeout: 10000,
+        query: {
+            APPID: OPENWEATHERMAP_API_KEY,
+            units: units,
+            lang: lang,
+            q: q
+        }
+    }).then(function(res) {
+        // Проверка наличия данных в ответе
+        if (res && res.data && res.data.list) {
+            $reactions.answer("Получен прогноз: " + JSON.stringify(res.data));  // Выводим данные в ответ
+            return res.data;
+        } else {
+            throw new Error("Нет данных в ответе от API.");
+        }
+    }).catch(function(err) {
+        // Логируем подробную ошибку
+        $reactions.answer("Ошибка при запросе прогноза: " + err.message);
+        $reactions.answer("Ответ сервера: " + JSON.stringify(err));  // Дополнительная информация об ошибке
+        throw new Error("Не могу получить прогноз. Проверьте запрос.");
+    });
+}
+
 function getWeatherForSpecificDate(units, lang, city, targetDate) {
     return $http.get("http://api.openweathermap.org/data/2.5/forecast?APPID=${APPID}&units=${units}&lang=${lang}&q=${city}", {
         timeout: 10000,
