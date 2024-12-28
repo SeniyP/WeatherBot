96733b955272df155868fdd6f4ea9f2487bbcb65
github-118610115 <github-118610115>
2024-12-28 12:36:20 +0000

    Edited files:     /src/functions.js

--- src/functions.js
+++ src/functions.js
@@ -1,9 +1,9 @@
 var OPENWEATHERMAP_API_KEY = $injector.api_key;
 
-function openWeatherMapCurrent(units, lang, q){
-return $http.get("http://api.openweathermap.org/data/2.5/weather?APPID=${APPID}&units=${units}&lang=${lang}&q=${q}", {
+function openWeatherMapCurrent(units, lang, q) {
+    return $http.get("http://api.openweathermap.org/data/2.5/weather?APPID=${OPENWEATHERMAP_API_KEY}&units=${units}&lang=${lang}&q=${q}", {
         timeout: 10000,
-        query:{
+        query: {
             APPID: OPENWEATHERMAP_API_KEY,
             units: units,
             lang: lang,
@@ -11,6 +11,8 @@ return $http.get("http://api.openweathermap.org/data/2.5/weather?APPID=${APPID}&
         }
     });
 }
+
+// Добавление функции для получения прогноза погоды
 function openWeatherMapForecast(units, lang, q, date) {
     return $http.get("http://api.openweathermap.org/data/2.5/forecast?APPID=${OPENWEATHERMAP_API_KEY}&units=${units}&lang=${lang}&q=${q}", {
         timeout: 10000,
@@ -22,13 +24,13 @@ function openWeatherMapForecast(units, lang, q, date) {
         }
     }).then(function(res) {
         if (res && res.data && res.data.list) {
-            console.log("Получен прогноз:", res.data);
+            $reactions.answer("Получен прогноз: " + JSON.stringify(res.data));  // Используем реакцию для вывода данных
             return res.data;
         } else {
             throw new Error("Нет данных в ответе от API.");
         }
     }).catch(function(err) {
-        console.error("Ошибка при запросе прогноза:", err);
+        $reactions.answer("Ошибка при запросе прогноза: " + err.message);  // Выводим ошибку через $reactions
         throw new Error("Не могу получить прогноз. Проверьте запрос.");
     });
 }
