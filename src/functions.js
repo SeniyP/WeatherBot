7c209b44c6d2901419abdc7f550be4cde8c93d90
github-118610115 <github-118610115>
2024-12-29 10:40:50 +0000

    Edited files:     /src/functions.js

--- src/functions.js
+++ src/functions.js
@@ -1,9 +1,9 @@
 var OPENWEATHERMAP_API_KEY = $injector.api_key;
 
 function openWeatherMapCurrent(units, lang, q) {
-    return $http.get("http://api.openweathermap.org/data/2.5/weather", {
+    return $http.get("https://api.openweathermap.org/data/2.5/weather", {
         timeout: 10000,
-        query: {
+        params: {
             APPID: OPENWEATHERMAP_API_KEY,
             units: units,
             lang: lang,
@@ -12,14 +12,14 @@ function openWeatherMapCurrent(units, lang, q) {
     });
 }
 
-function openWeatherMapForecast(units, lang, q) {
-    return $http.get("http://api.openweathermap.org/data/2.5/forecast", {
+function openWeatherMapCurrent(units, lang, q) {
+    return $http.get(`http://api.openweathermap.org/data/2.5/forecast?appid=${OPENWEATHERMAP_API_KEY}&units=${units}&lang=${lang}&q=${q}`, {
         timeout: 10000,
         query: {
-            APPID: OPENWEATHERMAP_API_KEY,
-            units: units,
-            lang: lang,
-            q: q
+            appid: OPENWEATHERMAP_API_KEY,  // API ключ
+            units: units,                   // Единицы измерения (метрические)
+            lang: lang,                     // Язык
+            q: q                            // Город
         }
     });
 }
