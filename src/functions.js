f18fbf5a293e818dc3438c2acff8001425750b33
github-118610115 <github-118610115>
2024-12-29 11:43:19 +0000

    Edited files:     /src/functions.js

--- src/functions.js
+++ src/functions.js
@@ -11,3 +11,15 @@ function openWeatherMapCurrent(units, lang, q) {
         }
     });
 }
+
+function openWeatherMapForecast(units, lang, q) {
+    return $http.get("http://api.openweathermap.org/data/2.5/forecast", {
+        timeout: 10000,
+        query: {
+            APPID: OPENWEATHERMAP_API_KEY,
+            units: units,
+            lang: lang,
+            q: q
+        }
+    });
+}
